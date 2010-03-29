fd35b5bb6d7e219d69d75190bb18c54b4522befb
Stephen Roller <scroller@ncsu.edu>
2010-03-29 02:51:38 -0400

    Make sure formatting lines up.

--- codegenerator.py
+++ codegenerator.py
@@ -396,11 +396,11 @@ def code5str(code5):
         if inst in ('LDC', 'LDA', 'LD', 'ST', 'JLT', 'JLE', 'JEQ', 
                     'JNE', 'JGE', 'JGT'):
             ln = linecounter.next() # line number
-            output.append("%5d: %-9s %d,%d(%d)\t\t%s" % (ln, inst, r, s, t, com))
+            output.append("%5d: %-9s %d,%2d(%d)\t\t%s" % (ln, inst, r, s, t, com))
         elif inst in ('HALT', 'IN', 'OUT', 'INB', 'OUTB', 'OUTC', 
                       'ADD', 'SUB', 'MUL', 'DIV', 'OUTNL'):
             ln = linecounter.next()
-            output.append("%5d: %-9s %d,%d,%d\t\t%s" % (ln, inst, r, s, t, com))
+            output.append("%5d: %-9s %d,%2d,%2d\t\t%s" % (ln, inst, r, s, t, com))
         elif inst == 'alloc':
             size = r
             for i in xrange(0, size):
