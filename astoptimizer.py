6558ed293ce7cb936caed53583b17808b6d08883
Stephen Roller <me@stephenroller.com>
2010-05-01 14:57:36 -0400

    Eliminate dead condition branches.

--- astoptimizer.py
+++ astoptimizer.py
@@ -63,10 +63,56 @@ def static_str_to_int(ast):
                 node.value = int(node.children[0].value)
                 node.children = []
 
+def cond_elimination(ast):
+    "Eliminates cond branches that will never execute."
+    for node in list(ast.postfix_iter()):
+        if node.node_type != 'cond':
+            continue
+        
+        children = node.children
+        i = 0
+        while i < len(children):
+            condition = children[i]
+            if condition.value == True:
+                # branch will always execute
+                del children[i+2:]
+                del children[i]
+            elif condition.value == False:
+                # dead branch, will never execute
+                del children[i + 1]
+                del children[i]
+            else:
+                i += 2
+        
+        if len(children) == 1:
+            # we have an else
+            node.become_child()
+        
+    
+    return False
+
 def optimize_ast(ast):
     static_str_to_int(ast)
     constant_propagation(ast)
+    cond_elimination(ast)
+
 
 if __name__ == '__main__':
     from ice9 import compile
-    print compile("write int('4') + 3 + int('8');")
+
+
+    source = """
+    if read < 4 ->
+    write 3 + 4;
+    write 8;
+    [] true ->
+    write 4;
+    [] else ->
+    write 5;
+    fi
+    """
+
+    print compile(source, False)
+    print "-" * 80
+    print
+    print compile(source, True)    
